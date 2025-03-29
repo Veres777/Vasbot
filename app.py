8a62a0d39acaea46f360a7895987c08dbaf55b57
Veres777 <spatial_traveller@proton.me>
2025-03-29 20:15:52 +0100

    Upravený bot – přidáno XYZ

--- app.py
+++ app.py
@@ -121,7 +121,11 @@ def check_user():
 if __name__ == "__main__":
     import os
     from dotenv import load_dotenv
+    import threading
+
     load_dotenv()
+
+    from bot import run_bot  # ujisti se, že importuješ správnou funkci z bot.py
 
     threading.Thread(target=run_bot, daemon=True).start()
 
